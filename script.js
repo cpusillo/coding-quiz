8d12ca7b8c9ec06bb1e63f4098e19cf66c06f732
cpusillo <cpusillo@gmail.com>
2020-10-04 11:46:29 -0400

    Finished application formatting and logic

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // Create our questions array, fill with actual question objects.
-var questions = [
-  {
+var questions = [{
     // Question 1
     title: "Commonly used data types DO NOT include:",
     choices: ["strings", "booleans", "alerts", "numbers"],
@@ -48,6 +47,8 @@ var correctCount = 0;
 var time = questions.length * 5;
 var intervalId;
 
+var name;
+
 // Render our first question to the user.
 renderQuestion();
 
@@ -59,7 +60,7 @@ renderQuestion();
 */
 function renderQuestion() {
 
-  if(time === 0){
+  if (time === 0) {
     endQuiz;
   }
   //create the time interval
@@ -77,7 +78,7 @@ function renderQuestion() {
 
   // Iterate through all of the answer choices and append them
   // to the page as LI elements.
-  for(var i = 0; i< choicesLength; i++){
+  for (var i = 0; i < choicesLength; i++) {
     // Create the actual li element.
     var questionListItem = document.createElement("li");
 
@@ -88,17 +89,17 @@ function renderQuestion() {
     questionListEl.append(questionListItem);
   }
 
-  if(time === 0){
+  if (time === 0) {
     endQuiz();
   }
 
-  }
+}
 
 /*
   The checkAnswer() function stores the user's click and uses input
   validation to ensure the user clicked on a valid li tag.
 */
-function checkAnswer(event){
+function checkAnswer(event) {
   // pause the timer
   clearInterval(intervalId);
 
@@ -106,13 +107,13 @@ function checkAnswer(event){
   var target = event.target;
 
   // validate that the user clicked on a valid li element.
-  if(target.matches("li")){
+  if (target.matches("li")) {
 
     // Store the actual text content of the li element in variable, 'selectedChoice'.
     var selectedChoice = event.target.textContent;
 
     // Compare selectedChoice to the correct answer from the current question object.
-    if(selectedChoice === questions[questionIndex].answer){
+    if (selectedChoice === questions[questionIndex].answer) {
       // If the selectedChoice is the correct answer, increment correctCount variable by 1.
       correctCount++;
 
@@ -144,34 +145,42 @@ function checkAnswer(event){
   array. It then calls renderQuestion() to render the new question
   to our user's screen.
 */
-function nextQuestion(){
-  // When all Qs ask end the quiz
-
+function nextQuestion() {
   // Clear our questionResultEl value.
   questionResultEl.textContent = "";
+
   // Increment our question index variable by 1.
   questionIndex++;
 
-  // Call our renderQuestion() function.
-  if(questionIndex === questions.length){
+  // Call our renderQuestion() function if there are questions left to ask
+  // If not, just end the quiz.
+  if (questionIndex === questions.length) {
     timer = 0;
     endQuiz();
   } else {
-  renderQuestion();}
+    renderQuestion();
+  }
 }
 
-function updateTime(){
+/*
+  The updateTime() function displays our running timer to 
+  the user. If the timer hits 0, end the quiz.
+*/
+function updateTime() {
   timerEl.textContent = time;
   // decrement time
-  time --;
+  time--;
   // if time is up end the quiz
-  if (time === 0){
+  if (time === 0) {
     endQuiz();
   }
 }
 
-
-function endQuiz(){
+/*
+  The endQuiz() function stops the timer and brings the
+  user to the high score screens.
+*/
+function endQuiz() {
 
   // clear interval
   clearInterval(intervalId);
@@ -182,11 +191,8 @@ function endQuiz(){
 
 
 function showHighScore() {
-
-  // Make room for the highscore area.
-  document.body.textContent = "";
   // Prompt user for name
-  var name = prompt("What is your name?");
+  name = prompt("What is your name?");
 
   // Call toHighScore() function to show the user's score.
   toHighScore();
@@ -197,17 +203,15 @@ function showHighScore() {
   names. Modularized so that users can skip past the game and
   go straight to the highscores from the main page.
 */
-function toHighScore(){
+function toHighScore() {
   clearInterval(intervalId);
   document.body.textContent = "";
-   // Check if there is anything in local storage and store it in variable
+  // Check if there is anything in local storage and store it in variable
   // nothing there high_scores = false;
   var high_score = localStorage.getItem("scores");
-  console.log(high_score);
-
 
   // if high scores doesn't exist
-  if(!high_score){
+  if (!high_score) {
     // create empty array
     high_score = [];
   } else {
@@ -225,37 +229,55 @@ function toHighScore(){
   localStorage.setItem("scores", JSON.stringify(high_score))
 
   // sort array
-  high_score.sort(function(a,b){
-    return b.score-a.score;
+  high_score.sort(function (a, b) {
+    return b.score - a.score;
   });
 
   // Write to DOM
-  
+
+  // Create our heading content.
   var heading = document.createElement("h2");
   heading.textContent = "Highscores";
 
+  // Create our containing div, container.
   var div = document.createElement("div");
   document.body.append(div);
-  div.setAttribute("class", "container");
+  div.setAttribute("class", "container highscores-display");
   div.append(heading);
 
+  // Create our containing UL.
   var contentUL = document.createElement("ul");
-
   contentUL.setAttribute("id", "highscore-list");
 
-  for(var i = 0; i < high_score.length; i++){
-    var contentLi= document.createElement("li");
-    contentLi.textContent = "Name: " + high_score[i].name + " Score: " + high_score[i].score;
-    contentUL.append(contentLi);
+  // Create our "Go Back" buttons.
+  var button = document.createElement("button");
+  button.setAttribute("class", "btn reload");
+  button.textContent = "Go back";
+
+  // Loop through our scores and names and display them to the user.
+  for (var i = 0; i < high_score.length; i++) {
+      var contentLi = document.createElement("li");
+
+      // Do not display scores if there is no username AND a zero score.
+      // This allows our users to skip right to the highscores without registering a score and adding garbage data.
+      if(high_score[i].name != null && high_score[i].score != 0){
+
+        contentLi.innerHTML = `<strong>Name: </strong> ${high_score[i].name} <strong>Score:</strong> ${high_score[i].score}`
+        //contentLi.textContent = `Name: ${high_score[i].name} \tScore: ${high_score[i].score}`
+        contentUL.append(contentLi);
+      }
   }
   div.append(contentUL);
+  div.append(button);
+
+  button.addEventListener("click", function(){
+    location.reload()
+  });
 
 }
 
 
- // Listen for a click on any of the LI elements, check their answer with the checkAnswer() function.
-  questionListEl.addEventListener("click", checkAnswer);
+// Listen for a click on any of the LI elements, check their answer with the checkAnswer() function.
+questionListEl.addEventListener("click", checkAnswer);
 
-  viewHighScores.addEventListener("click", function(){
-    toHighScore();
-  });
+viewHighScores.addEventListener("click", toHighScore);
